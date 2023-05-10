e0d15e5a64934b2f1a8f5914a8fbd27b45e553e0
Razvan <manea.razvan.valentin@gmail.com>
2023-05-10 21:43:14 +0300

    Switching the data models to repository pattern

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -20,8 +20,7 @@ const webPort = "80"
 var counts int64
 
 type Config struct {
-	DB     *sql.DB
-	Models data.Models
+	Repo data.Repository
 }
 
 func main() {
@@ -34,10 +33,9 @@ func main() {
 	}
 
 	// set up config
-	app := Config{
-		DB:     conn,
-		Models: data.New(conn),
-	}
+	app := Config{}
+
+	app.setupRepo(conn)
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -86,4 +84,9 @@ func connectToDB() *sql.DB {
 		time.Sleep(2 * time.Second)
 		continue
 	}
+}
+
+func (app *Config) setupRepo(conn *sql.DB) {
+	db := data.NewPostgresRepository(conn)
+	app.Repo = db
 }
